60975f4e04f43b3b4f66bd50d10847fc28deae0e
Osl00 <74931793+Osl00@users.noreply.github.com>
2023-03-17 16:40:08 +0100

    caracterisation du moteur
    programme de test et mesure.
    le python fait le plots

--- CaracterisationRobot/moteur/moyennevitesse.py
+++ CaracterisationRobot/moteur/moyennevitesse.py
@@ -1,22 +1,44 @@
 import os
-
+import matplotlib.pyplot as plt
+import numpy as np
 os.listdir
 
-mesdir="./mesure"
+mesdir="./CaracterisationRobot/moteur/mesure/"
 
-listdir=os.listdir(mesdir)
 
-f=open("mesureMoteur_pwm80.txt","r")
-ff=f.readlines()
+def mesure_moyene(filepath):
+    f=open(filepath,"r")
+    ff=f.readlines()
+    m=0
+    n=0
+    mes=[]
+    for i in ff:
+        i=float(i)
+        m+=i
+        n+=1
+        mes+=[i]
+    return [mes , m/(n*int(filepath.split('_')[2].split('-')[0])),int(filepath.split('_')[1].split('-')[1])]
 
-ff=ff[1:]
-m=0
-n=0
-for i in ff:
-    i=float(i)
-    m+=i
-    n+=1
+moyene=[mesure_moyene(mesdir+i) for i in os.listdir(mesdir)]
 
-m=m/(n*5)
+toplote=[[],[],[]]
+toplotem=[[],[],[]]
+for x in moyene:
 
-print("Moyenne en cm/s :    ",m)
+    for y in x[0] :
+        toplote[0]=toplote[0]+[y]
+        toplote[1]=toplote[1]+[x[2]]
+        toplote[2]=toplote[2]+[(0,0,1)]
+for x in moyene:
+    toplotem[0]=toplotem[0]+[x[1]]
+    toplotem[1]=toplotem[1]+[x[2]]
+    toplotem[2]=toplotem[2]+[(1,0,0)]
+
+
+fig,ax=plt.subplots(2,1)
+ax[0].scatter(toplote[1],toplote[0],8,toplote[2])
+ax[0].set_xlabel("pwm")
+ax[1].scatter(toplotem[1],toplotem[0],32,toplotem[2])
+ax[1].set_xlabel("pwm")
+
+plt.show()
